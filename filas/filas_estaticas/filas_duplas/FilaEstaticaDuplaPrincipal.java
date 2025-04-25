a0e0c6e010438c77124c1268ebb874de4703538c
Mescxll <202411190002@ifba.edu.br>
2025-04-25 17:44:15 -0300

    Adição fila estática dupla principal.

--- filas/filas_estaticas/filas_duplas/FilaEstaticaDuplaPrincipal.java
+++ filas/filas_estaticas/filas_duplas/FilaEstaticaDuplaPrincipal.java
@@ -2,6 +2,41 @@ package filas_duplas;
 
 public class FilaEstaticaDuplaPrincipal {
     public static void main(String[] args) {
-        // A fazer
+        FilaEstaticaDupla fd = new FilaEstaticaDupla(10);
+
+        fd.enfileirarFim(1);
+        fd.enfileirarFim(2);
+        fd.enfileirarFim(3);
+        fd.enfileirarInicio(4);
+        fd.enfileirarInicio(5);
+        fd.enfileirarInicio(6);
+        fd.enfileirarFim(7);
+        fd.enfileirarInicio(8);
+        fd.enfileirarInicio(9);
+
+        System.out.println("Fila de Frente para Trás: " + fd.imprimirFrenteTras());
+        System.out.println("Fila de Trás para Frente: " + fd.imprimirTrasFrente());
+        
+        System.out.println("Desenfileirar Fim...");
+        fd.desenfileirarFim();
+
+        System.out.println("Fila de Frente para Trás: " + fd.imprimirFrenteTras());
+        System.out.println("Fila de Trás para Frente: " + fd.imprimirTrasFrente());
+
+        System.out.println("Desenfileirar Inicio...");
+        fd.desenfileirarInicio();
+
+        System.out.println("Fila de Frente para Trás: " + fd.imprimirFrenteTras());
+        System.out.println("Fila de Trás para Frente: " + fd.imprimirTrasFrente());
+    
+        System.out.println("Atualizando Inicio e Fim (-1 e 0)...");
+        fd.atualizarInicio(-1);
+        fd.atualizarFim(0);
+
+        System.out.println("Fila de Frente para Trás: " + fd.imprimirFrenteTras());
+        System.out.println("Fila de Trás para Frente: " + fd.imprimirTrasFrente());
+    
+        System.out.println("Frente: " + fd.frente());
+        System.out.println("Trás: " + fd.tras());
     }
 }
